645d275320f21910581c4e657e9920f4ea1e2fde
Pawel Datko <92950825+PawelDat@users.noreply.github.com>
2022-11-26 23:48:12 +0100

    Search Component  & Api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
-import "./css/main.css"
-const App = () => {
-  return (
-    <div></div>
-  )
-}
+import React from "react";
+import Search from "./components/search";
 
-export default App
+import "./css/main.css";
+
+const App = () => {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+    </div>
+  );
+};
+
+export default App;
