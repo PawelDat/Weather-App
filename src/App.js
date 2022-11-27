64fafc5c153e9ba0505b2b210da640a9637654d5
Pawel Datko <92950825+PawelDat@users.noreply.github.com>
2022-11-27 23:19:52 +0100

    Completed Daily weather Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./components/api";
 import Search from "./components/search";
 import CurrentWeather from "./components/currentweather";
 import "./css/main.css";
 
 const App = () => {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setforecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
-  }
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setforecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
+  };
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather/>
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 };
--- src/components/currentweather.js
+++ src/components/currentweather.js
@@ -1,33 +1,37 @@
 import React from "react";
-import img from "../assets/01d.png";
-const CurrentWeather = () => {
+
+const CurrentWeather = ({data}) => {
   return (
     <div className="weather">
       <div className="top">
         <div className="paragraphs">
-          <p className="city">Katowice</p>
-          <p className="weather-description">Rainy</p>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
-        <img src={img} alt="weather" className="weather-icon" />
+        <img
+          src={`icons/${data.weather[0].icon}.png`}
+          alt="weather"
+          className="weather-icon"
+        />
       </div>
       <div className="bottom">
-        <p className="temperature">18°C</p>
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">19°C</span>
+            <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-value">3m/s</span>
+            <span className="parameter-value">{data.wind.speed}m/s</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">15%</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">15 hPa</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
